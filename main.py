b94ed36602aedc979ee66aa06ea6ee3f2b3d413c
mishi <mishi.beraia@gmail.com>
2024-02-27 01:04:13 +0400

    Initial commit.

---
+++ main.py
@@ -0,0 +1,18 @@
+import streamlit as st
+import requests
+
+KEY = "4UQKbsQUeRvtVHwspDC0MhC7QEPikUWZ9McbeE7M"
+url = f"https://api.nasa.gov/planetary/apod?api_key={KEY}&thumbs=True"
+response = requests.get(url)
+
+cont = response.json()
+img_link = cont['thumbnail_url']
+with open ("image.jpg", "wb") as file:
+    r2 = requests.get(img_link)
+    file.write(r2.content)
+
+st.title(cont["title"])
+st.image("image.jpg")
+content = cont["explanation"]
+st.write(content)
+
